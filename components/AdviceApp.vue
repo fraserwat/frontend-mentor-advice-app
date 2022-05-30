5a9f4b0f5fc140f39a3831817539818f5228eba0
Fraser Watt <fsrwatt@gmail.com>
2022-05-30 13:28:04 +0100

    First commit

---
+++ components/AdviceApp.vue
@@ -0,0 +1,24 @@
+<!-- Please remove this file from your project -->
+<template>
+  <section class="[ app-container ] [ bg-dark-grayish-blue max-width-540 pad-900 align-center rad-15 ]">
+    <div class="advice">
+      <h1 class="color-neon-green text-300 gap-bottom-600">Advice #{{ adviceNo }}</h1>
+      <h2 class="color-light-cyan">“{{ adviceText }}”</h2>
+    </div>
+    <button class="[ reroll ] [ bg-neon-green ]" @click="rerollAdviceCall"/>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial',
+  data () {
+    return {
+      adviceNo: 117,
+      adviceText: "It is easy to sit up and take notice, what's difficult is getting up and taking action."
+    }
+  },
+  methods: {
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <main class="[ body ] [ bg-dark-blue flex-center pad-400 ]">
+    <advice-app />
+  </main>
+</template>
+
+<script>
+import AdviceApp from '../components/AdviceApp.vue';
+
+export default {
+  name: 'IndexPage',
+  components: { AdviceApp },
+}
+</script>
